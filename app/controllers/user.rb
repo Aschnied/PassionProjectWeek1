35a430b5df17f58187c33e0c3047ee9d1f8500a2
Andrew Schnieder <andrewschnieder@gmail.com>
2015-02-17 15:44:37 -0800

    made significant additions to css

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,30 @@
-get '/:user_id' do
-  "This is where users will view their decks."
+get '/users' do
+  "show all users"
 end
+
+get '/users/new' do
+  "show html form for creating a new user"
+end
+
+post '/users' do
+  "a new user was just created!"
+end
+
+get '/users/:id' do
+  @user = User.find(session[:user_id])
+  @decks = @user.decks
+  erb :user
+end
+
+get '/users/:id/edit' do
+  "show a form to edit a user with id = #{params[:id]}"
+end
+
+put '/users/:id' do
+  "the user with id #{params[:id]} was just updated!"
+end
+
+delete '/users/:id' do
+  "the user with id #{params[:id]} was just deleted!"
+end
+
